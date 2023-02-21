3d90e3d3f8da760c65f4668f784cd213dce8ae8e
Mikhael Martin <mikhael.martin@gmail.com>
2023-02-22 00:12:36 +0700

    fix type for argparser

--- test_LPRNet.py
+++ test_LPRNet.py
@@ -24,11 +24,11 @@ import os
 
 def get_parser():
     parser = argparse.ArgumentParser(description='parameters to train net')
-    parser.add_argument('--img_size', default=[94, 24], help='the image size')
+    parser.add_argument('--img_size', default=[94, 24], nargs='+', type=int, help='the image size')
     parser.add_argument('--test_img_dirs', default="./data/test", help='the test images path')
-    parser.add_argument('--dropout_rate', default=0, help='dropout rate.')
-    parser.add_argument('--lpr_max_len', default=8, help='license plate number max length.')
-    parser.add_argument('--test_batch_size', default=100, help='testing batch size.')
+    parser.add_argument('--dropout_rate', default=0, type=float, help='dropout rate.')
+    parser.add_argument('--lpr_max_len', default=8, type=int, help='license plate number max length.')
+    parser.add_argument('--test_batch_size', default=100, type=int, help='testing batch size.')
     parser.add_argument('--phase_train', default=False, type=bool, help='train or test phase flag.')
     parser.add_argument('--num_workers', default=8, type=int, help='Number of workers used in dataloading')
     parser.add_argument('--cuda', default=True, type=bool, help='Use cuda to train model')
